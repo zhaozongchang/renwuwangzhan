d36dff51cd1198e9216d154088579a142738eb63
zhaozongchang <xingyingsuifeng@gmail.com>
2018-02-05 17:19:52 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -24,17 +25,10 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
+
   end
 
   def update
-    @task = Task.find(params[:id])
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
 
     if @task.update(task_params)
     redirect_to tasks_path
@@ -44,11 +38,6 @@ class TasksController < ApplicationController
 end
 
   def destroy
-    @task = Task.find(params[:id])
-    
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
 
     @task.destroy
     redirect_to tasks_path,notice: "删除成功"
@@ -56,6 +45,14 @@ end
 
 
   private
+
+  def find_task_and_check_permission
+    @task = Task.find(params[:id])
+
+    if current_user != @task.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
 
   def task_params
     params.require(:task).permit(:title, :description, :unacommpolished, :buckle, :deduction, :score)
